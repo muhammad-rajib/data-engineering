6f18b67bea2d48b48de5141b5ee7c7b62624d8f2
muhammad-rajib <rajibhossain8521@gmail.com>
2022-02-11 18:51:17 +0600

    updated

--- projects/mflix-etl-job/collect_data.py
+++ projects/mflix-etl-job/collect_data.py
@@ -5,11 +5,11 @@ and return the list of collected Data.
 from utils import connect_with_mongodb
 
 
-def create_movies_data_list():
+def collect_movies_data_into_list():
     client = connect_with_mongodb()
     movies_data = client.sample_mflix.movies
     
-    # movies list
+    # prepare movies list
     movies_list = []
     for movie in movies_data.find():
         movies_list.append(movie)
