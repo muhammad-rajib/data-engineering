2f68d1dad47a601b79dd45a40521e1ed7ab173a8
muhammad-rajib <rajibhossain8521@gmail.com>
2022-02-11 11:47:41 +0600

    functions for connect with mongodb atlas cluster is done.

--- projects/mflix-etl-job/utils.py
+++ projects/mflix-etl-job/utils.py
@@ -1,12 +1,11 @@
 """
 Utility functions for ETL job of mflix.
 """
-
 # import required libraries
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
-from pyspark.sql import SparkSession
+# from pyspark.sql import SparkSession
 
 
 def get_spark_session(dev_mode, app_name):
@@ -21,4 +20,18 @@ def mongodb_env_dict():
     }
 
 def connect_with_mongodb():
-    pass
+    cred_dict = mongodb_env_dict()
+    user = cred_dict['user']
+    password = cred_dict['password']
+    db_name = cred_dict['db_name']
+
+    mongo_uri = f"mongodb+srv://{user}:{password}@mflix-cluster.buei4.mongodb.net/{db_name}?retryWrites=true&w=majority"
+    mongo_client = MongoClient(str(mongo_uri))
+    
+    return mongo_client
+
+
+if __name__ == "__main__":
+    client = connect_with_mongodb()
+    for name in client.list_database_names():
+        print(name)
