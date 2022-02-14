8141c21173e573c68dc88cef117ecd5eb35770e3
muhammad-rajib <rajibhossain8521@gmail.com>
2022-02-14 16:51:50 +0600

    added function docstring and collect db, table name using utils function

--- projects/sales-etl-job/collect_data.py
+++ projects/sales-etl-job/collect_data.py
@@ -1,18 +1,22 @@
-"""
-Extract supplies sales data from MongoDB Atlas Cluster
-and return the list of collected Data.
-"""
 from utils import connect_with_mongodb
+from utils import mongodb_env_dict2
 
 
 def collect_data_into_list():
+    """
+    function: return sales data list.
+    :Make a connection with mongodb atlas cluster
+    :using connect_with_mongodb functions of utils.
+    :Then extract data from cluster db and load into the list.
+    """
+    env_dict = mongodb_env_dict2()
+
     client = connect_with_mongodb()
-    sales_data = client.sample_supplies.sales
+    sales_data = client.env_dict['db_name'].env_dict['table_name']
     
     # prepare sales data list
     sales_data_list = []
     for item in sales_data.find():
-        del item['_id']
         sales_data_list.append(item)
 
     return sales_data_list
