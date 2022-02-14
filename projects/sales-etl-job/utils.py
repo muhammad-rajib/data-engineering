17a4efd245a61387a82aafb0288c29ad8ad555ae
muhammad-rajib <rajibhossain8521@gmail.com>
2022-02-14 16:55:35 +0600

    added docstring in all functions & added mongodb_env_dict2, spark_session_env_dict functions with other litle bit modification

--- projects/sales-etl-job/utils.py
+++ projects/sales-etl-job/utils.py
@@ -1,5 +1,5 @@
 """
-Utility functions for ETL job of mflix.
+Utility functions for ETL job of sales data.
 """
 # import required libraries
 import os
@@ -8,11 +8,75 @@ from pymongo import MongoClient
 from pyspark.sql import SparkSession
 
 
-def get_spark_session():
-
+def spark_session_env_dict():
+    """
+    function: return dictionary wrapped with spark-session credentials.
+    :credentials are loaded from .env file.
+    """
     load_dotenv()
-    dev_mode = os.getenv('spark_session_mode')
-    app_name = os.getenv('spark_app_name')
+    return {
+        'mode'     : os.getenv('spark_session_mode'),
+        'app_name' : os.getenv('spark_app_name')
+    }
+
+def s3_env_dict():
+    """
+    function: return dictionary wrapped with aws s3 credentials.
+    :credentials are loaded from .env file.
+    """
+    load_dotenv()
+    return {
+        'file_name'     : os.getenv('s3_file_name'),
+        'bucket_name'   : os.getenv('s3_bucket_name'),
+        'aws_iam_role'  : os.getenv('s3_aws_iam_role')
+    }
+
+def redshift_env_dict():
+    """
+    function: return dictionary wrapped with aws redshift credentials.
+    :credentials are loaded from .env file.
+    """
+    load_dotenv()
+    return {
+        'endpoint'  : os.getenv('redshift_endpoint'),
+        'user'      : os.getenv('redshift_user'),
+        'password'  : os.getenv('redshift_password'),
+        'port'      : os.getenv('redshift_port'),
+        'db_name': os.getenv('redshift_db_name')
+    }
+
+def mongodb_env_dict():
+    """
+    function: return dictionary wrapped with mongodb credentials.
+    :credentials are loaded from .env file.
+    """
+    load_dotenv()
+    return {
+        'user'      : os.getenv('mongodb_user'),
+        'password'  : os.getenv('mongodb_password'),
+        'db_name'   : os.getenv('mongodb_name'),
+        'host'      : os.getenv('mongodb_host_address')
+    }
+
+def mongodb_env_dict2():
+    """
+    function: return dictionary wrapped with mongodb credentials.
+    :credentials are loaded from .env file.
+    """
+    load_dotenv()
+    return {
+        'db_name'   : os.getenv('mongodb_name'),
+        'table_name': os.getenv('mongodb_table_name')
+    }
+
+def get_spark_session():
+    """
+    function: return newly created spark session.
+    :required informations are loaded from .env file.
+    """
+    cred_dict = spark_session_env_dict() 
+    dev_mode = cred_dict['mode']
+    app_name = cred_dict['app_name']
 
     if dev_mode == 'DEV':
         spark = SparkSession. \
@@ -30,31 +94,18 @@ def get_spark_session():
         return spark
     return
 
-def redshift_env_dict():
-    load_dotenv()
-    return {
-        'endpoint'  : os.getenv('redshift_endpoint'),
-        'user'      : os.getenv('redshift_user'),
-        'password'  : os.getenv('redshift_password'),
-        'port'      : os.getenv('redshift_port'),
-        'table_name': os.getenv('redshift_table_name')
-    }
-
-def mongodb_env_dict():
-    load_dotenv()
-    return {
-        'user'      : os.getenv('mongodb_user'),
-        'password'  : os.getenv('mongodb_password'),
-        'db_name'   : os.getenv('mongodb_name')
-    }
-
 def connect_with_mongodb():
+    """
+    function: return dictionary wrapped with aws s3 credentials.
+    :required informations are loaded from .env file by functions.
+    """
     cred_dict = mongodb_env_dict()
     user = cred_dict['user']
     password = cred_dict['password']
     db_name = cred_dict['db_name']
+    db_host = cred_dict['host']
 
-    mongo_uri = f"mongodb+srv://{user}:{password}@mflix-cluster.buei4.mongodb.net/{db_name}?retryWrites=true&w=majority"
+    mongo_uri = f"mongodb+srv://{user}:{password}{db_host}{db_name}?retryWrites=true&w=majority"
     mongo_client = MongoClient(mongo_uri)
     
     return mongo_client
