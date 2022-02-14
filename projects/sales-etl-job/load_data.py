85e1850e01e0c8024658d1dc514d19e902136889
muhammad-rajib <rajibhossain8521@gmail.com>
2022-02-14 23:11:19 +0600

    completed

--- projects/sales-etl-job/load_data.py
+++ projects/sales-etl-job/load_data.py
@@ -4,41 +4,47 @@ Load Data into Redshift (AWS Data Warehouse).
 # import required libraries
 import boto3
 import psycopg2
-from utils import s3_env_dict, redshift_env_dict
 from io import StringIO 
 
 
-def load_data_into_s3_bucket(sales_df):
+def load_data_into_s3_bucket(sales_df, s3_cred):
+    """
+    load sales into aws s3 bucket.
 
-    # write dataframe as a csv to S3
-    filename = 'salesrecords.csv'
-    bucket_name = 'redshift-sales-data'
-    csv_buffer = StringIO()
+    :param --> sales_df: pandas dataframe of sales data
+    :required informations are loaded from .env file by utils function.
+    """
+
+    filename    = s3_cred['file_name']
+    bucket_name = s3_cred['bucket_name']
+
+    csv_buffer  = StringIO()
     sales_df.to_csv(csv_buffer)
 
     client = boto3.client('s3')
 
-    response = client.put_object(
-        ACL='private',
-        Body=csv_buffer.getvalue(),
-        Bucket = bucket_name,
-        Key = filename
+    client.put_object(
+        ACL     = 'private',
+        Body    = csv_buffer.getvalue(),
+        Bucket  = bucket_name,
+        Key     = filename
     )
 
 
-def load_data_into_redshift(sales_df):
+def load_data_s3_to_redshift(s3_cred, redshift_cred):
+    """
+    load sales data from aws s3 bucket to redshift.
 
-    load_data_into_s3_bucket(sales_df)
-
-    env = redshift_env_dict()
-
+    :param --> redshift_cred: redshift credentials wrapped with dictionary
+    :after successfull execution, data will be available in redshift data-warehouse
+    """
     conn = psycopg2.connect(
-    host='redshift-sales-cluster.cmypa1emn3fe.us-east-1.redshift.amazonaws.com',
-    user='awsuser',
-    port=5439,
-    password= env['password'],
-    dbname='dev'
-    )
+                host        = redshift_cred['endpoint'],
+                user        = redshift_cred['user'],
+                port        = redshift_cred['port'],
+                password    = redshift_cred['password'],
+                dbname      = redshift_cred['db_name']
+            )
 
     cur = conn.cursor()
 
@@ -46,15 +52,15 @@ def load_data_into_redshift(sales_df):
     conn.commit()
 
     try:
-        cur.execute(f"""DROP TABLE IF EXISTS salesinfos;""")
+        cur.execute(f"""DROP TABLE IF EXISTS {redshift_cred['table_name']};""")
     except psycopg2.Error as e:
         print("Error: table drop time.")
         print(e)
 
     try:
         cur.execute(
-            """
-            create table salesinfos (
+            f"""
+            create table {redshift_cred['table_name']} (
             id INTEGER,
             purchase_date VARCHAR(20),
             item VARCHAR(30),
@@ -71,25 +77,19 @@ def load_data_into_redshift(sales_df):
     except psycopg2.Error as e:
         print("Error: Issue creating table")
         print(e)
-
-    sql = f'''COPY salesdata FROM 's3://redshift-sales-data/salesrecords.csv'
-        credentials 'aws_access_key_id='AKIAU77AZSSHIBCAXGPP;aws_secret_access_key=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'
-        CSV;
-        '''
     
-    sql2 = f'''
-        COPY salesinfos FROM 's3://redshift-sales-data/salesrecords.csv'
-        credentials 'aws_iam_role=arn:aws:iam::343531951246:role/redshift-s3-access'
+    sql = f'''
+        COPY {redshift_cred['table_name']} FROM 's3://{s3_cred['bucket_name']}/{s3_cred['file_name']}'
+        credentials 'aws_iam_role={s3_cred['aws_iam_role']}'
         DATEFORMAT 'yyyy-mm-dd'
         IGNOREHEADER 1
         CSV
         '''
 
     try:
-        cur.execute(sql2)
+        cur.execute(sql)
     except psycopg2.Error as e:
         print("Error: In copy time")
         print(e)
     
     conn.commit()
-
